27524d523749036dd12123423ed464228cb7ef2b
GUANTAI001 <jpguantai@gmail.com>
2023-05-28 10:39:57 +0300

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,62 +1,69 @@
 import {
-    AppstoreOutlined,
-    ShopOutlined,
-    ShoppingCartOutlined,
-    UserOutlined,
-  } from "@ant-design/icons";
-  import { Menu } from "antd";
-  import { useEffect, useState } from "react";
-  import { useLocation, useNavigate } from "react-router-dom";
-  import '../App.css';
+  AppstoreOutlined,
+  ShopOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import '../App.css';
+import Profile from "../images/images.jpeg";
+import { Link } from "react-router-dom";
+
 
 
 
 const Sidebar = () => {
-    const location = useLocation();
-    const [selectedKeys, setSelectedKeys] = useState("/");
-  
-    useEffect(() => {
-      const pathName = location.pathname;
-      setSelectedKeys(pathName);
-    }, [location.pathname]);
-  
-    const navigate = useNavigate();
-    return (
-        <div className="sidebar-content">
 
-      <div className="">
-        <Menu className="SideMenuVertical" mode="vertical"
-          onClick={(item) => {
-            //item.key
-            navigate(item.key);
-          }}
-          selectedKeys={[selectedKeys]}
-          items={[
-            {
-              label: "Dashbaord",
-              icon: <AppstoreOutlined />,
-              key: "/",
-            },
-            {
-              label: "Inventory",
-              key: "/inventory",
-              icon: <ShopOutlined />,
-            },
-            {
-              label: "Orders",
-              key: "/orders",
-              icon: <ShoppingCartOutlined />,
-            },
-            {
-              label: "Customers",
-              key: "/customers",
-              icon: <UserOutlined />,
-            },
-          ]}
-        ></Menu>
-      </div>
+
+
+  return (
+    <div className="bg-slate-900">
+      <div style={{ height: '100vh', display: 'flex' }}>
+        <div style={{ position: 'fixed', top: 0 }}>
+          <div className="p-10">
+            <img className="rounded-full w-40 h-40" src={Profile} alt="Profile" />
+          </div>
+
+          <div className="flex flex-col m-2">
+            <Link to="/" className="flex flex-row mb-8">
+              < AppstoreOutlined
+                className="text-3xl text-white hover:text-gray-400 mr-2" />
+              <h1 className="text-start text-2xl font-bold text-white hover:text-gray-400">
+                Dashboard
+              </h1>
+            </Link>
+            <Link to="/inventory" className="flex flex-row mb-8">
+              < ShopOutlined
+                className="text-3xl text-white hover:text-gray-400 mr-2" />
+              <h1 className="text-start text-2xl font-bold text-white hover:text-gray-400">
+                Inventory
+              </h1>
+            </Link>
+            <Link to="/orders" className="flex flex-row mb-8">
+              < ShoppingCartOutlined
+                className="text-3xl text-white hover:text-gray-400 mr-2" />
+              <h1 className="text-start text-2xl font-bold text-white hover:text-gray-400">
+                Orders
+              </h1>
+            </Link>
+            <Link to="/customers" className="flex flex-row mb-8">
+              < UserOutlined
+                className="text-3xl text-white hover:text-gray-400 mr-2" />
+              <h1 className="text-start text-2xl font-bold text-white hover:text-gray-400">
+                Customers
+              </h1>
+            </Link>
+            
+          </div>
+
         </div>
-    );
-  }
+      </div>
+    </div>
+
+
+  );
+}
 
 export default Sidebar;
